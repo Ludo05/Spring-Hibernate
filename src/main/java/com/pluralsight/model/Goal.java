10c41565440752def8f053bda70cf2d5e8c093cc
LewisWilliams <lewiswilliams1501@gmail.com>
2018-08-19 17:07:32 +0100

    Spring with Hibernate

--- src/main/java/com/pluralsight/model/Goal.java
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -2,9 +2,19 @@ package com.pluralsight.model;
 
 import org.hibernate.validator.constraints.Range;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigInteger;
 
 
+@Entity
 public class Goal {
+
+    @Id
+    @GeneratedValue
+	private BigInteger id;
 
 	@Range(min = 1, max = 120)
 	private int minutes;
@@ -16,5 +26,12 @@ public class Goal {
 	public void setMinutes(int minutes) {
 		this.minutes = minutes;
 	}
-	
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
 }
